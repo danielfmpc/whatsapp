07b1f4be7c511761869b7c60a67ed9764e0b7460
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-09-11 17:32:15 -0300

    Criando area de login

---
+++ lib/Login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
