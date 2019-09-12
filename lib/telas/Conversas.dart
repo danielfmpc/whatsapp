9f58ab8c20bf606eebf4abdcfc0a219dda12a0da
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-09-12 11:28:44 -0300

    Corrigindo as rotas

---
+++ lib/telas/Conversas.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Mensagem extends StatefulWidget {
+  @override
+  _MensagemState createState() => _MensagemState();
+}
+
+class _MensagemState extends State<Mensagem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
